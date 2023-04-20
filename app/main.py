65a785b51283ca821127aa57f2c5f381bb5c20e6
= <ywen9901@gmail.com>
2023-04-20 13:25:56 +0800

    :sparkles: CORS

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import uvicorn
 from fastapi import FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
@@ -18,6 +19,19 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
     )
+
+origins = [
+    "https://designeditor.epl.tw"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 app.include_router(block.router)
 app.include_router(connection.router)
